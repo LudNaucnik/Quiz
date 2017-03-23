abac234c6d737a4006e7e0a350b6fab114d027f4
LudNaucnik <bajraktarovljube@gmail.com>
2017-03-23 01:25:55 +0100

    Addede Something

--- app/src/main/java/com/ljube/bajtaktarov/quiz/MainActivity.java
+++ app/src/main/java/com/ljube/bajtaktarov/quiz/MainActivity.java
@@ -1,12 +1,26 @@
 package com.ljube.bajtaktarov.quiz;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -14,9 +28,9 @@ import butterknife.OnEditorAction;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int CancelReqCode = 8906, FCReqCode = 8907, CapCityReqCode = 8908;
+    public static final int CancelReqCode = 8906, FCReqCode = 8907, CapCityReqCode = 8908, PickPhotoReqCode = 8909;
     Button FCButton, CapitalCityButton, NewGameButton;
-    TextView PointsTextView;
+    TextView PointsTextView, imgidText, QuestionText, AnswerText;
     int Points = 0;
 
 
@@ -35,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+
     View.OnClickListener newGameMain = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -45,7 +60,61 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.AddButton)
     public void AddData() {
-        Toast.makeText(this, "Works", Toast.LENGTH_SHORT).show();
+        CreateChooseCategoryAlert();
+    }
+
+
+    void CreateChooseCategoryAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Choose Category")
+                .setItems(R.array.categories, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (which == 0) {
+                            CreateAddDataAlert();
+                        }
+                        if (which == 1) {
+                            CreateAddDataAlert();
+                        }
+                    }
+                });
+        builder.create();
+        builder.show();
+    }
+
+    void CreateAddDataAlert() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View myView = inflater.inflate(R.layout.add_data_alert, null);
+        QuestionText  = (TextView) myView.findViewById(R.id.question);
+        AnswerText = (TextView) myView.findViewById(R.id.answer);
+        imgidText = (TextView) myView.findViewById(R.id.imgid);
+        Button choosePictire = (Button) myView.findViewById(R.id.choosePictureButton);
+        choosePictire.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, PickPhotoReqCode);
+            }
+        });
+        builder.setView(myView)
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        String answerString = AnswerText.getText().toString();
+                        String questionString = QuestionText.getText().toString();
+                        String imgidString = imgidText.getText().toString();
+                        Toast.makeText(MainActivity.this,answerString+questionString+imgidString,Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        builder.create();
+        builder.show();
     }
 
     View.OnClickListener FCButtonOnClick = new View.OnClickListener() {
@@ -53,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             Intent intent = new Intent(MainActivity.this, CreateQuizActivity.class);
             intent.putExtra("DataType", "fcdata");
-            intent.putExtra("QuestionStructure","Which FC is from ");
+            intent.putExtra("QuestionStructure", "Which FC is from ");
             intent.putExtra("Title", "Football Clubs");
             intent.putExtra("ReqCode", FCReqCode);
             startActivityForResult(intent, FCReqCode);
@@ -65,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             Intent intent = new Intent(MainActivity.this, CreateQuizActivity.class);
             intent.putExtra("DataType", "capitalcitydata");
-            intent.putExtra("QuestionStructure","Which country has capital city ");
+            intent.putExtra("QuestionStructure", "Which country has capital city ");
             intent.putExtra("Title", "Capital Cities");
             intent.putExtra("ReqCode", CapCityReqCode);
             startActivityForResult(intent, CapCityReqCode);
@@ -84,6 +153,10 @@ public class MainActivity extends AppCompatActivity {
         }
         if (requestCode == CancelReqCode) {
             PointsTextView.setText("Points " + String.valueOf(Points));
+        }
+        if (requestCode == PickPhotoReqCode) {
+            Uri selectedImageUri = data.getData();
+            imgidText.setText(selectedImageUri.getPath());
         }
     }
 }
