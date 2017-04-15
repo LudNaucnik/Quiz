b5b3e49b6e845058b1edb5dda71e07ac04084950
LudNaucnik <bajraktarovljube@gmail.com>
2017-04-15 22:44:56 +0200

    App now can read high scores

--- app/src/main/java/com/ljube/bajtaktarov/quiz/HighScoreActivity.java
+++ app/src/main/java/com/ljube/bajtaktarov/quiz/HighScoreActivity.java
@@ -8,10 +8,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class HighScoreActivity extends AppCompatActivity {
+
+    ListView HighScoreList;
+    List<HighscoreData> highscoreArraylist = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +33,43 @@ public class HighScoreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_high_score);
         setTitle("High Scores");
         centerTitle();
+        HighScoreList = (ListView) findViewById(R.id.HighScoresList);
+    }
+
+    void readHighScore() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("HighScores");
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                HighscoreData highscore = new HighscoreData();
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    highscore = child.getValue(HighscoreData.class);
+                    highscoreArraylist.add(highscore);
+                }
+                Collections.sort(highscoreArraylist, new Comparator<HighscoreData>() {
+                    public int compare(HighscoreData score1, HighscoreData score2) {
+                        if (Integer.parseInt(score1.Points) < Integer.parseInt(score2.Points)) {
+                            return +1;
+                        } else if (Integer.parseInt(score1.Points) > Integer.parseInt(score2.Points)) {
+                            return -1;
+                        } else {
+                            return 0;
+                        }
+                    }
+                });
+                for (HighscoreData data : highscoreArraylist) {
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Intent intent = new Intent();
+                setResult(MainActivity.CancelReqCode, intent);
+                finish();
+            }
+        });
     }
 
     @Override
